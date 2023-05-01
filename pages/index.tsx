fa96aa48a525b353cf8a0bb0e66311a4db8e3444
Psami-wondah <okechukwusamuel16@gmail.com>
2023-05-01 05:14:03 +0100

    choke

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,31 @@
+import Image from "next/image";
+import { Nunito } from "next/font/google";
+import h from "@/public/heart.svg";
+
+const inter = Nunito({ subsets: ["latin"] });
+
 export default function Home() {
-  return <main>Developing.....</main>;
+  return (
+    <main className="w-screen h-screen flex items-center justify-center  bg-gradient-to-br from-purple-500 to-red-500">
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-5xl font-bold text-center text-gray-800">
+            Amayi is beautiful
+          </h1>
+          <p className="mt-4 text-lg text-center text-gray-600">
+            And she deserves all the love in the world
+          </p>
+          <div className="mt-8 flex justify-center gap-4 items-center">
+            <a
+              href="#"
+              className="px-6 py-2 text-lg font-bold text-white bg-pink-500 rounded-full hover:bg-pink-600"
+            >
+              Show your love
+            </a>
+            <Image src={h} alt="heart" className="w-max heart" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
